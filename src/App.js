9dfd0184bc7801ebbcccf1970eea6f2bcb1f8d4b
vickzil <nwakwyevictor@gmail.com>
2024-05-07 22:09:29 +0100

    fixed errors and issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
- import {BrowserRouter as  Router } from 'react-router-dom';
-import './App.css';
- import routes from './Routes/router';
- 
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import routes from "./Routes/router";
+
 function App() {
   return (
-    <Router>
-    {routes}
-  </Router>
+    <Router>{routes}</Router>
+    // <div>
+    //   <p>Hello World</p>
+    // </div>
   );
 }
 
